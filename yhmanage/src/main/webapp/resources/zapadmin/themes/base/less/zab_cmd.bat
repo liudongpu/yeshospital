12112ae1a4dd5cd24dd5aea8a10b1f473051138a
srnpr <srnpr@qh-liudp>
2014-08-21 00:08:37 +0800

    init

---
+++ yhmanage/src/main/webapp/resources/zapadmin/themes/base/less/zab_cmd.bat
@@ -0,0 +1,13 @@
+
+
+
+
+start zab_cmd_run.bat zab_less_green.less  ../../green/css/zab_css.css
+start zab_cmd_run.bat zab_less_orange.less ../../orange/css/zab_css.css
+start zab_cmd_run.bat zab_less_blue.less   ../../blue/css/zab_css.css
+start zab_cmd_run.bat zab_less_cyan.less   ../../cyan/css/zab_css.css
+start zab_cmd_run.bat zab_less_pink.less   ../../pink/css/zab_css.css
+start zab_cmd_run.bat zab_less_red.less   ../../red/css/zab_css.css
+start zab_cmd_run.bat zab_less_jasper.less   ../../jasper/css/zab_css.css
+
+
