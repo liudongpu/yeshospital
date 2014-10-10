18fe1f269c9923b28fbb80f99d18569e38320012
srnpr <srnpr@qh-liudp>
2014-10-11 00:35:58 +0800

    a

--- yeshospital/src/main/java/com/srnpr/yeshospital/post/PostSiheal.java
+++ yeshospital/src/main/java/com/srnpr/yeshospital/post/PostSiheal.java
@@ -1,18 +1,49 @@
 package com.srnpr.yeshospital.post;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.srnpr.zapcom.baseclass.BaseClass;
+import com.srnpr.zapcom.basehelper.FormatHelper;
+import com.srnpr.zapdata.dbdo.DbUp;
+import com.srnpr.zapweb.helper.WebHelper;
+import com.srnpr.zapweb.helper.WebSessionHelper;
 
 public class PostSiheal extends BaseClass {
 
-	
-	public String process()
-	{
+	public String process() {
+		HttpServletRequest hRequest = WebSessionHelper.create().upHttpRequest();
+
+		StringBuffer info = new java.lang.StringBuffer();
+		try {
+			InputStream in = hRequest.getInputStream();
+			BufferedInputStream buf = new BufferedInputStream(in);
+			byte[] buffer = new byte[1024];
+			int iRead;
+
+			while ((iRead = buf.read(buffer)) != -1) {
+				info.append(new String(buffer, 0, iRead, "UTF-8"));
+			}
+		} catch (UnsupportedEncodingException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		String sInfo = info.toString();
+
+		String sLogCodeString = WebHelper.upCode("LC");
+
+		DbUp.upTable("yh_log_post").insert("log_code", sLogCodeString,
+				"post_info", sInfo, "create_time", FormatHelper.upDateTime());
+
 		return "1";
 	}
-	
-	
-	
-	
-	
-	
+
 }
