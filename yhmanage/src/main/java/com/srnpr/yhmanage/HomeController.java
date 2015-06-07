bf541159ada67ac774c777d4aa34f4bb61c1f1ec
srnpr <srnpr@qh-liudp>
2015-06-07 20:24:41 +0800

    a

--- yhmanage/src/main/java/com/srnpr/yhmanage/HomeController.java
+++ yhmanage/src/main/java/com/srnpr/yhmanage/HomeController.java
@@ -1,24 +1,16 @@
 package com.srnpr.yhmanage;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.srnpr.yeshospital.pages.ExportQrcode;
-import com.srnpr.zapweb.webexport.ExportChart;
 import com.srnpr.zapweb.webfactory.UserFactory;
 import com.srnpr.zapweb.webmethod.RootControl;
 
