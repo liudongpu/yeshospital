cee0da06fe55300f6cf918a0a1b4dcf546997853
srnpr <srnpr@qh-liudp>
2015-08-13 01:06:02 +0800

    a

--- yeshospital/src/main/java/com/srnpr/yeshospital/support/EtSupport.java
+++ yeshospital/src/main/java/com/srnpr/yeshospital/support/EtSupport.java
@@ -1,5 +1,7 @@
 package com.srnpr.yeshospital.support;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.srnpr.yeshospital.et.rsync.EtRsyncAddDev;
 import com.srnpr.zapcom.basemodel.MDataMap;
 import com.srnpr.zapdata.dbdo.DbUp;
@@ -14,7 +16,8 @@ public class EtSupport {
 			String sRString = new EtRsyncAddDev().upHttp(new MDataMap(
 					"devSerial", map.get("gateway_no")));
 
-			map.inAllValues("reg_result", sRString, "flag_reg", "1");
+			map.inAllValues("reg_result", sRString, "flag_reg",
+					StringUtils.equals(sRString, "succeed") ? "1" : "0");
 
 			DbUp.upTable("yh_et_gateway").dataUpdate(map,
 					"reg_result,flag_reg", "zid");
