ef917eea756e05702768806062862c26bb19b889
srnpr <srnpr@qh-nt>
2016-04-10 10:04:16 +0800

    a

--- yeshospital/src/main/java/com/srnpr/yeshospital/support/WxSupport.java
+++ yeshospital/src/main/java/com/srnpr/yeshospital/support/WxSupport.java
@@ -1,22 +1,47 @@
 package com.srnpr.yeshospital.support;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.srnpr.zapcom.baseclass.BaseClass;
 import com.srnpr.zapcom.basehelper.FormatHelper;
 import com.srnpr.zapcom.basesupport.WebClientSupport;
+import com.srnpr.zapweb.helper.WebSessionHelper;
 
 public class WxSupport extends BaseClass {
 
 	public String upOpenId(String sCode) {
 
 		String sReturn = "";
-
-		String sUrl = FormatHelper.formatString(
-				bConfig("yeshospital.wx_url_openid"),
-				bConfig("yeshospital.wx_appid"),
+		String sUrl = FormatHelper.formatString(bConfig("yeshospital.wx_url_openid"), bConfig("yeshospital.wx_appid"),
 				bConfig("yeshospital.wx_secret"), sCode);
 
+		String sRequestUrl = WebSessionHelper.create().upHttpRequest().getRequestURL().toString();
+		if (StringUtils.isNotBlank(sRequestUrl)) {
+
+			if (StringUtils.contains(sRequestUrl, ".wei.")) {
+
+				sRequestUrl = StringUtils.substringBefore(sRequestUrl, ".");
+
+				if (StringUtils.contains(sRequestUrl, "/")) {
+					sRequestUrl = StringUtils.substringAfterLast(sRequestUrl, "/");
+				}
+				if (StringUtils.contains(sRequestUrl, "%2f")) {
+					sRequestUrl = StringUtils.substringAfterLast(sRequestUrl, "%2f");
+				}
+
+				if (StringUtils.isNotBlank(sRequestUrl)) {
+					sUrl = FormatHelper.formatString(bConfig("yeshospital.wx_url_openid"),
+							bConfig("yeshospital.wei_appid_" + sRequestUrl),
+							bConfig("yeshospital.wei_secret_" + sRequestUrl), sCode);
+				}
+
+			}
+
+		}
+
+		bLogInfo(965805284, sUrl);
 		try {
 			String sResponse = WebClientSupport.create().doGet(sUrl);
 
