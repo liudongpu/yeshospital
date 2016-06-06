d8a7a0b0257b8dc5afe40d19dc3e570eb0473af7
srnpr <srnpr@qh-nt>
2016-06-06 14:11:27 +0800

    a

--- yeshospital/src/main/java/com/srnpr/yeshospital/func/FuncAddTourDetail.java
+++ yeshospital/src/main/java/com/srnpr/yeshospital/func/FuncAddTourDetail.java
@@ -1,8 +1,11 @@
 package com.srnpr.yeshospital.func;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.srnpr.yeshospital.support.ProcRefreshSupport;
 import com.srnpr.zapcom.basemodel.MDataMap;
 import com.srnpr.zapdata.dbdo.DbUp;
+import com.srnpr.zapweb.helper.WebHelper;
 import com.srnpr.zapweb.webdo.WebConst;
 import com.srnpr.zapweb.webfunc.FuncAdd;
 import com.srnpr.zapweb.webfunc.RootFunc;
@@ -15,22 +18,27 @@ public class FuncAddTourDetail extends RootFunc {
 		MWebResult mWebResult = new MWebResult();
 		MDataMap mAddMaps = mDataMap.upSubMap(WebConst.CONST_WEB_FIELD_NAME);
 
-		if (mWebResult.upFlagTrue()) {
+		String sLockCode = WebHelper.addLock(100, mAddMaps.get("member_code"));
+		if (StringUtils.isNotBlank(sLockCode)) {
+			if (mWebResult.upFlagTrue()) {
 
-			if (DbUp.upTable("yh_tour_order_detail").count("tour_code", mAddMaps.get("tour_code"), "member_code",
-					mAddMaps.get("member_code")) > 0) {
+				if (DbUp.upTable("yh_tour_order_detail").count("tour_code", mAddMaps.get("tour_code"), "member_code",
+						mAddMaps.get("member_code")) > 0) {
 
-				mWebResult.inErrorMessage(965806004);
+					mWebResult.inErrorMessage(965806004);
+
+				}
 
 			}
 
-		}
+			if (mWebResult.upFlagTrue()) {
+				mWebResult = new FuncAdd().funcDo(sOperateUid, mDataMap);
 
-		if (mWebResult.upFlagTrue()) {
-			mWebResult = new FuncAdd().funcDo(sOperateUid, mDataMap);
+				new ProcRefreshSupport().refreshCountAgree(mAddMaps);
 
-			new ProcRefreshSupport().refreshCountAgree(mAddMaps);
-
+			}
+		} else {
+			mWebResult.inErrorMessage(965806004);
 		}
 
 		return mWebResult;
