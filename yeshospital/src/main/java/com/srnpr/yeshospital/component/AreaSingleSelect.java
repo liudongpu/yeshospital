261e2d8d55b613f9137851cc1f54db18535df912
srnpr <srnpr@qh-nt>
2016-09-02 23:34:12 +0800

    a

---
+++ yeshospital/src/main/java/com/srnpr/yeshospital/component/AreaSingleSelect.java
@@ -0,0 +1,18 @@
+package com.srnpr.yeshospital.component;
+
+import com.srnpr.zapcom.basemodel.MDataMap;
+import com.srnpr.zapweb.webcomponent.ComponentWindowSingle;
+import com.srnpr.zapweb.webmodel.MWebField;
+
+public class AreaSingleSelect  extends ComponentWindowSingle {
+
+	
+	public String upText(MWebField mWebField, MDataMap mDataMap, int iType) {
+
+		mWebField
+				.setSourceParam("zw_s_max_select=1&zw_s_source_tableinfo=ze_area_code|area_code|area_name&zw_s_source_page=page_chart_chart_v_ze_area_code");
+
+		return upShowText(mWebField, mDataMap, iType);
+	}
+	
+}
