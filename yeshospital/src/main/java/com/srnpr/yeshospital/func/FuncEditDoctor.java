bef5aefc4aa2e455a3b9c2961317d49c8db8ea0f
srnpr <srnpr@qh-nt>
2017-05-11 10:38:58 +0800

    a

---
+++ yeshospital/src/main/java/com/srnpr/yeshospital/func/FuncEditDoctor.java
@@ -0,0 +1,29 @@
+package com.srnpr.yeshospital.func;
+
+import com.srnpr.zapcom.basemodel.MDataMap;
+import com.srnpr.zapdata.dbdo.DbUp;
+import com.srnpr.zapweb.webdo.WebConst;
+import com.srnpr.zapweb.webfunc.FuncEdit;
+import com.srnpr.zapweb.webfunc.RootFunc;
+import com.srnpr.zapweb.webmodel.MWebResult;
+
+public class FuncEditDoctor extends RootFunc {
+	public MWebResult funcDo(String sOperateUid, MDataMap mDataMap) {
+
+		MWebResult mWebResult = new MWebResult();
+
+		if (mWebResult.upFlagTrue()) {
+			mWebResult = new FuncEdit().funcDo(sOperateUid, mDataMap);
+		}
+
+		if (mWebResult.upFlagTrue()) {
+			MDataMap mAddMaps = mDataMap.upSubMap(WebConst.CONST_WEB_FIELD_NAME);
+			MDataMap map = DbUp.upTable("yh_tour_order_drug").one("uid", mAddMaps.get("uid"));
+			if (map != null && map.size() > 0) {
+				DbUp.upTable("fm_form_scope").delete("user_code", map.get("user_code"));
+			}
+		}
+
+		return mWebResult;
+	}
+}
